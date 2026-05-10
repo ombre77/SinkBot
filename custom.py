1a4b6188842b15c804ee104b1d095a3e4abfe5b9
Odysseus <odysseus.77@hotmail.com>
2026-05-10 19:51:43 +0200

    clean start

---
+++ custom.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import json
+
+class Message:
+    def __init__(self,title:str,color:discord.Color,desc:str=None):
+        self.title=title
+        self.color=color
+        self.desc=desc
+
+        self.embed=discord.Embed(
+            title=self.title,
+            color=self.color,
+            description=self.desc
+        )
+        self.embed.set_footer("developed by Odysseus :3")
+    
+    def add_category(self,name:str,content:str,inline=True):
+        self.embed.add_field(name=name,value=content,inline=inline)
+
+    def footer(self,content:str):
+        self.embed.set_footer(text=content)
+
+    def render(self):
+        return self.embed
+
+class JSON_map:
+    def __init__(self,path:str):
+        self.path=path
+        self.map={}
+    
+    def load(self):
+        with open(self.path,"r") as f:
+            file=json.load(f)
+        self.map=file.get("map")
+    
